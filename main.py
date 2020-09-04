4315757a0245bce4f72d05fc613615a9e1088022
Kai Anter <kai.anter@web.de>
2020-09-04 22:28:51 +0200

    Edit: More outputs for user during calculation

--- main.py
+++ main.py
@@ -11,23 +11,27 @@ def StartMinFinderTests():
     WriteResultLine([timestamp])
 
     for l in TESTLENGTHS:
-        MeasureMinFinder(l)
+        MeasureMinFinder(l, 2)
 
     WriteResultLine(["-"*30])
 
 
-def MeasureMinFinder(arrayLength):
+def MeasureMinFinder(arrayLength, runCount):
+    print("Starting measurement for length " + str(arrayLength) + "...")
     timesum = 0
     
-    for i in range(10):
+    for i in range(runCount):
+        print("run " + str(i+1), end = "...")
         unsortedArray = NumberFactory.GetUnsortedSet(arrayLength)
         start = timer()
         sortedArray = MinFinder(unsortedArray)
         end = timer()
         timesum += (end-start) # add current iteration to sum
+    print("")
 
-    average = timesum / 10
+    average = timesum / runCount
     print("Average time needed for {0} iterations with length {1}: {2}".format(10, arrayLength, average))
+    print("-"*30)
     WriteResultLine([arrayLength, average])
     
 
