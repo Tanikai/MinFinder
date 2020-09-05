07d321508be47f61b885c904a8198acdc8502eff
Kai Anter <kaianter29@gmail.com>
2020-09-05 18:46:55 +0200

    Added: Functions for sorted and reverse sorted sets

--- NumberFactory.py
+++ NumberFactory.py
@@ -3,6 +3,15 @@ from random import shuffle
 # This module is used for generating a set of random numbers.
 
 def GetUnsortedSet(i_count):
-    List = [i for i in range(1, i_count+1)]
-    shuffle(List)
-    return List
+    numberSet = [i for i in range(1, i_count+1)]
+    shuffle(numberSet)
+    return numberSet
+
+def GetSortedSet(i_count):
+    numberSet = [i for i in range(1, i_count+1)]
+    return numberSet
+
+def GetReverseSortedSet(i_count):
+    numberSet = [i for i in range(1, i_count+1)]
+    numberSet.reverse()
+    return numberSet
